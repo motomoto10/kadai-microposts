3f0918faff722f0596374b35ccf17acce492f2eb
takuro.moto <kpdvox8370@yahoo.co.jp>
2020-12-12 13:34:21 +0000

    migrate 複数形

--- database/migrations/2020_12_06_134900_create_favorites_table.php
+++ database/migrations/2020_12_06_134900_create_favorites_table.php
@@ -20,7 +20,7 @@ class CreateFavoritesTable extends Migration
             $table->timestamps();
             
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('micropost_id')->references('id')->on('micropost')->onDelete('cascade');
+            $table->foreign('micropost_id')->references('id')->on('microposts')->onDelete('cascade');
         
             // user_idとfollow_idの組み合わせの重複を許さない
             $table->unique(['user_id', 'micropost_id']);
